309b07c4c5f5d8064fdefe375dc8af6bbfa7a9cf
Rics <danil.klementev.29@gmail.com>
2025-08-25 13:39:21 +0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/widgets/Hero/Hero";
+import Footer from "@/widgets/Footer/Footer";
 
 export default function Home() {
     return (
         <div className='container'>
             <Hero />
+            <Footer />
         </div>
     );
 }
