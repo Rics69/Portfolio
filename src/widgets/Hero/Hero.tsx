f7b4dcca792401df149b5cbdfde701a266b646ff
Rics <danil.klementev.29@gmail.com>
2025-08-26 11:32:16 +0300

    feat: add socials

--- src/widgets/Hero/Hero.tsx
+++ src/widgets/Hero/Hero.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 const Hero = () => {
     return (
         <div>
             <img src="/avatar.jpg" alt="Данил Клементьев"/>
             <h1>Я Клементьев Данил</h1>
+            <ul>
+                <li><Link href='/'>Telegram</Link></li>
+                <li><Link href='/'>Github</Link></li>
+                <li><Link href='/'>Mail</Link></li>
+            </ul>
         </div>
     );
 };
